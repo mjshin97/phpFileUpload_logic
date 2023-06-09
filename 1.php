d2b046e4095f04f125eda322cd43536643346399
root <mjshin97@naver.com>
2023-06-09 18:16:36 +0900

    first commit

---
+++ 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+</head>   
+<body>
+<?php
+ini_set("display_errors", "1");
+$uploaddir = './upload/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {//
+    echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
+} else {
+    print "파일 업로드 공격의 가능성이 있습니다!\n";
+}
+echo '자세한 디버깅 정보입니다:';
+print_r($_FILES);
+print "</pre>";      
+?>
+<img src="./upload/<?=$_FILES['userfile']['name']?>"><!--  -->
+</body>
+</html>
--- create.php
+++ create.php
@@ -20,23 +20,22 @@
         $query = "select * from test_board where id='$id' ";
         $result = mysqli_query($conn, $query);
         $data = mysqli_fetch_array($result);
-
     ?>
-
-
 
     <form action="process_create.php" method="POST">
         <?php if(isset($_GET['id'])) { ?>
             <input type="hidden" name="id" value="<?=$id?>">
             <p><input type="text" name="title"  value="<?=$data['title']?>"  placeholder="title" ></p>
-            <p><textarea name="description" placeholder="description"><?=$data['description']?></textarea></p> 
+            <p><textarea name="description" placeholder="description"><?=$data['description']?></textarea></p>
+            File upload <input type="file" name = "file"></input>
             <p><input type="submit"></p>
         <?php } else { ?>
             <p><input type="text" name="title"  placeholder="title" ></p>
-            <p><textarea name="description" placeholder="description"></textarea></p> 
+            <p><textarea name="description" placeholder="description"></textarea></p>
+            File upload <input type="file" name = "file"></input>
             <p><input type="submit"></p>
         <?php } ?>
-        
     </form>
+
 </body>
 </html> 
